5db572b3eb98911ea855c2b61340bee839b4e0ad
Qiongjie <tqjustc@gmail.com>
2017-05-24 15:01:58 -0700

    vgg
    Signed-off-by: Qiongjie <tqjustc@gmail.com>

--- vgg_like.py
+++ vgg_like.py
@@ -13,3 +13,21 @@ y_test = keras.utils.to_categorical(np.random.randint(10, size=(20, 1)), num_cla
 model=Sequential()
 model.add(Conv2D(32, (3,3), activation='relu', input_shape=(100,100,3)))
 model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(units=256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=10, activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd)
+
+model.fit(x_train, y_train, batch_size=32, epochs=10)
+score = model.evaluate(x_test, y_test, batch_size=32)
